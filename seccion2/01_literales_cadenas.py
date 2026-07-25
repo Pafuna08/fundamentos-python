e97fa6111f2c19c1158828716cb7d33ccebc3531
Pablo <fuenmayor0908@hotmail.com>
2026-07-25 12:46:30 -0500

    Se agrega laboratorio de la sección 2

---
+++ seccion2/01_literales_cadenas.py
@@ -0,0 +1,12 @@
+mensaje = "Python facilita la automatizacion de tareas."
+lenguaje = "Python"
+version = 3
+es_facil_de_leer = True
+
+print("Mensaje:", mensaje)
+print("Lenguaje: " + lenguaje)
+print("Repeticion:", lenguaje * 3)
+print("Cantidad de caracteres:", len(mensaje))
+print("Primer caracter:", mensaje[0])
+print("Version:", version)
+print("Es facil de leer:", es_facil_de_leer)
